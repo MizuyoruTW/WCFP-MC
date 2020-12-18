2082f9f5863b96d24c53dd9f1cd44e6c33bf357a
MizuyoruTW <mizuyoru.tw@gmail.com>
2020-12-18 21:46:14 +0800

    upgrade database ver to 2

--- app/src/main/java/com/example/wcfp_mc/CategoryDBHelper.java
+++ app/src/main/java/com/example/wcfp_mc/CategoryDBHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class CategoryDBHelper  extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "Class";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
     public CategoryDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
